fffab84fb4ac1c6546850f1e90866a0dea828cff
Sam Emerson <samerson1001@gmail.com>
2015-11-13 11:53:31 -0600

    movement

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ import random
 
 num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 	   11, 12, 13, 14, 15, 16, 17, 18, 19]
-
 random.shuffle(num)
-
 maze = [
 		[num[0],[num[4],num[5],num[1]]],
 	    [num[1],[num[0],num[7],num[2]]],
@@ -28,5 +26,39 @@ maze = [
 	    [num[19],[num[18],num[15],num[12]]],
 	   ]
 
-print("You are in room ", maze[0][0])
-print("You can go to ", maze[0][1])
+pos = 0
+
+while 1:
+	print("You are in room ", maze[pos][0])
+	print("You can go to ", maze[pos][1])
+	newpos = input("Go to: ")
+	newpos = int(newpos)
+	
+	test = 0
+	i = 0
+	while i < 3:
+		if maze[pos][1][i] == newpos:
+			test += 1
+		i += 1
+
+	if test > 0:
+		x = 0
+		while x < 20:
+			if maze[x][0] == newpos:
+				pos = x
+				break
+			x += 1
+	else:
+		print("Invalid room")
+
+
+
+
+
+
+
+
+
+
+
+
